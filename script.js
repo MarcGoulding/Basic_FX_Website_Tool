7f2bd972ee38da1ba5ed7599aeb3bea04117b2ef
MarcGoulding <gouldingmarc@gmail.com>
2020-12-15 23:25:07 +0100

    version 1.0

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 /* Initialise */
 let s1 = document.getElementById("180-pc-button");
-// Event listeners:
+/* Event listeners */
 s1.addEventListener("click", toggleRecipe);
 function toggleRecipe(){
   /* Hide and un-hide recipe divs */
   console.log("recipe selected");
 }
 
-// Create a "close" button and append it to each list item
+/* ------------------------------------------------------------------------- */
+
+/* Create a "close" button and append it to each list item */
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -18,7 +20,7 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
+/* Click on a close button to hide the current list item */
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -28,7 +30,7 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
+/* Add a "checked" symbol when clicking on a list item */
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
@@ -39,10 +41,10 @@ list.addEventListener('click', function(ev) {
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("notes-input").value;
-  var t = document.createTextNode(inputValue);
+  var input = document.getElementById("notes-input");
+  var t = document.createTextNode(input.value);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (input.value === '') {
     console.log("attempted to add note with no input text.");
   } else {
     document.getElementById("notes-ul").appendChild(li);
@@ -62,6 +64,8 @@ function newElement() {
     }
   }
 }
+
+/* ------------------------------------------------------------------------- */
 
 loadData();
 
@@ -96,8 +100,246 @@ function loadData(){
 
     /* Display seconds in bottom-right corner */
     let sec = document.getElementById("seconds");
-    console.log(sec);
     sec.innerHTML = s;
 }
 
 setInterval(loadData, 1000);
+
+/* ------------------------------------------------------------------------- */
+
+/* initialise strength analysis output text elements */
+currencyEls = {
+  eur: document.getElementById("eur-txt"),
+  usd: document.getElementById("usd-txt"),
+  chf: document.getElementById("chf-txt"),
+  gbp: document.getElementById("gbp-txt"),
+  cad: document.getElementById("cad-txt"),
+  aud: document.getElementById("aud-txt"),
+  nzd: document.getElementById("nzd-txt"),
+  jpy: document.getElementById("jpy-txt")
+};
+/*
+strength values for each currency pair.
+1 = long
+-1 = short
+0 = range
+2 = error
+*/
+strengths = {
+  EURUSD:2,
+  GBPUSD:2,
+  NZDUSD:2,
+  AUDUSD:2,
+  USDCAD:2,
+  USDCHF:2,
+  USDJPY:2,
+  EURGBP:2,
+  EURAUD:2,
+  EURCAD:2,
+  EURJPY:2,
+  EURNZD:2,
+  EURCHF:2,
+  GBPJPY:2,
+  GBPNZD:2,
+  GBPAUD:2,
+  GBPCHF:2,
+  GBPCAD:2,
+  AUDNZD:2,
+  AUDCAD:2,
+  AUDCHF:2,
+  AUDJPY:2,
+  NZDCAD:2,
+  NZDJPY:2,
+  NZDCHF:2,
+  CADJPY:2,
+  CADCHF:2,
+  CHFJPY:2
+};
+
+function generateStrengthOutput() {
+  currencyEls.eur.innerText = "err";
+  currencyEls.usd.innerText = "err";
+  currencyEls.chf.innerText = "err";
+  currencyEls.gbp.innerText = "err";
+  currencyEls.cad.innerText = "err";
+  currencyEls.aud.innerText = "err";
+  currencyEls.nzd.innerText = "err";
+  currencyEls.jpy.innerText = "err";
+
+  /* Check all pairs for each currency */
+  /* USD */
+  strength = 0;
+  strength = strengths.EURUSD==-1? strength+1 : strength;
+  strength = strengths.EURUSD== 1? strength-1 : strength;
+  strength = strengths.GBPUSD==-1? strength+1 : strength;
+  strength = strengths.GBPUSD== 1? strength-1 : strength;
+  strength = strengths.AUDUSD==-1? strength+1 : strength;
+  strength = strengths.AUDUSD== 1? strength-1 : strength;
+  strength = strengths.NZDUSD==-1? strength+1 : strength;
+  strength = strengths.NZDUSD== 1? strength-1 : strength;
+  strength = strengths.USDCHF== 1? strength+1 : strength;
+  strength = strengths.USDCHF==-1? strength-1 : strength;
+  strength = strengths.USDJPY== 1? strength+1 : strength;
+  strength = strengths.USDJPY==-1? strength-1 : strength;
+  strength = strengths.USDCAD== 1? strength+1 : strength;
+  strength = strengths.USDCAD==-1? strength-1 : strength;
+  currencyEls.usd.innerText = strength;
+  /* EUR */
+  strength = 0;
+  strength = strengths.EURUSD==+1? strength+1 : strength;
+  strength = strengths.EURUSD==-1? strength-1 : strength;
+  strength = strengths.EURGBP==+1? strength+1 : strength;
+  strength = strengths.EURGBP==-1? strength-1 : strength;
+  strength = strengths.EURAUD==+1? strength+1 : strength;
+  strength = strengths.EURAUD==-1? strength-1 : strength;
+  strength = strengths.EURCAD==+1? strength+1 : strength;
+  strength = strengths.EURCAD==-1? strength-1 : strength;
+  strength = strengths.EURCHF==+1? strength+1 : strength;
+  strength = strengths.EURCHF==-1? strength-1 : strength;
+  strength = strengths.EURJPY==+1? strength+1 : strength;
+  strength = strengths.EURJPY==-1? strength-1 : strength;
+  strength = strengths.EURNZD==+1? strength+1 : strength;
+  strength = strengths.EURNZD==-1? strength-1 : strength;
+  currencyEls.eur.innerText = strength;
+  /* GBP */
+  strength = 0;
+  strength = strengths.GBPUSD==+1? strength+1 : strength;
+  strength = strengths.GBPUSD==-1? strength-1 : strength;
+  strength = strengths.EURGBP==-1? strength+1 : strength;
+  strength = strengths.EURGBP==+1? strength-1 : strength;
+  strength = strengths.GBPAUD==+1? strength+1 : strength;
+  strength = strengths.GBPAUD==-1? strength-1 : strength;
+  strength = strengths.GBPCAD==+1? strength+1 : strength;
+  strength = strengths.GBPCAD==-1? strength-1 : strength;
+  strength = strengths.GBPCHF==+1? strength+1 : strength;
+  strength = strengths.GBPCHF==-1? strength-1 : strength;
+  strength = strengths.GBPJPY==+1? strength+1 : strength;
+  strength = strengths.GBPJPY==-1? strength-1 : strength;
+  strength = strengths.GBPNZD==+1? strength+1 : strength;
+  strength = strengths.GBPNZD==-1? strength-1 : strength;
+  currencyEls.gbp.innerText = strength;
+  /* AUD */
+  strength = 0;
+  strength = strengths.AUDUSD==+1? strength+1 : strength;
+  strength = strengths.AUDUSD==-1? strength-1 : strength;
+  strength = strengths.GBPAUD==-1? strength+1 : strength;
+  strength = strengths.GBPAUD==+1? strength-1 : strength;
+  strength = strengths.EURAUD==+1? strength+1 : strength;
+  strength = strengths.EURAUD==-1? strength-1 : strength;
+  strength = strengths.AUDCAD==+1? strength+1 : strength;
+  strength = strengths.AUDCAD==-1? strength-1 : strength;
+  strength = strengths.AUDCHF==+1? strength+1 : strength;
+  strength = strengths.AUDCHF==-1? strength-1 : strength;
+  strength = strengths.AUDJPY==+1? strength+1 : strength;
+  strength = strengths.AUDJPY==-1? strength-1 : strength;
+  strength = strengths.AUDNZD==+1? strength+1 : strength;
+  strength = strengths.AUDNZD==-1? strength-1 : strength;
+  currencyEls.aud.innerText = strength;
+  /* CAD */
+  strength = 0;
+  strength = strengths.USDCAD==-1? strength+1 : strength;
+  strength = strengths.USDCAD==+1? strength-1 : strength;
+  strength = strengths.GBPCAD==-1? strength+1 : strength;
+  strength = strengths.GBPCAD==+1? strength-1 : strength;
+  strength = strengths.AUDCAD==-1? strength+1 : strength;
+  strength = strengths.AUDCAD==+1? strength-1 : strength;
+  strength = strengths.EURCAD==-1? strength+1 : strength;
+  strength = strengths.EURCAD==+1? strength-1 : strength;
+  strength = strengths.CADCHF==+1? strength+1 : strength;
+  strength = strengths.CADCHF==-1? strength-1 : strength;
+  strength = strengths.CADJPY==+1? strength+1 : strength;
+  strength = strengths.CADJPY==-1? strength-1 : strength;
+  strength = strengths.NZDCAD==-1? strength+1 : strength;
+  strength = strengths.NZDCAD==+1? strength-1 : strength;
+  currencyEls.cad.innerText = strength;
+  /* NZD */
+  strength = 0;
+  strength = strengths.NZDUSD==+1? strength+1 : strength;
+  strength = strengths.NZDUSD==-1? strength-1 : strength;
+  strength = strengths.GBPNZD==-1? strength+1 : strength;
+  strength = strengths.GBPNZD==+1? strength-1 : strength;
+  strength = strengths.AUDNZD==-1? strength+1 : strength;
+  strength = strengths.AUDNZD==+1? strength-1 : strength;
+  strength = strengths.NZDCAD==+1? strength+1 : strength;
+  strength = strengths.NZDCAD==-1? strength-1 : strength;
+  strength = strengths.NZDCHF==+1? strength+1 : strength;
+  strength = strengths.NZDCHF==-1? strength-1 : strength;
+  strength = strengths.NZDJPY==+1? strength+1 : strength;
+  strength = strengths.NZDJPY==-1? strength-1 : strength;
+  strength = strengths.EURNZD==-1? strength+1 : strength;
+  strength = strengths.EURNZD==+1? strength-1 : strength;
+  currencyEls.nzd.innerText = strength;
+  /* JPY */
+  strength = 0;
+  strength = strengths.USDJPY==-1? strength+1 : strength;
+  strength = strengths.USDJPY==+1? strength-1 : strength;
+  strength = strengths.GBPJPY==-1? strength+1 : strength;
+  strength = strengths.GBPJPY==+1? strength-1 : strength;
+  strength = strengths.AUDJPY==-1? strength+1 : strength;
+  strength = strengths.AUDJPY==+1? strength-1 : strength;
+  strength = strengths.CADJPY==-1? strength+1 : strength;
+  strength = strengths.CADJPY==+1? strength-1 : strength;
+  strength = strengths.CHFJPY==-1? strength+1 : strength;
+  strength = strengths.CHFJPY==+1? strength-1 : strength;
+  strength = strengths.NZDJPY==-1? strength+1 : strength;
+  strength = strengths.NZDJPY==+1? strength-1 : strength;
+  strength = strengths.EURJPY==-1? strength+1 : strength;
+  strength = strengths.EURJPY==+1? strength-1 : strength;
+  currencyEls.jpy.innerText = strength;
+  /* CHF */
+  strength = 0;
+  strength = strengths.USDCHF==-1? strength+1 : strength;
+  strength = strengths.USDCHF==+1? strength-1 : strength;
+  strength = strengths.GBPCHF==-1? strength+1 : strength;
+  strength = strengths.GBPCHF==+1? strength-1 : strength;
+  strength = strengths.AUDCHF==-1? strength+1 : strength;
+  strength = strengths.AUDCHF==+1? strength-1 : strength;
+  strength = strengths.CADCHF==-1? strength+1 : strength;
+  strength = strengths.CADCHF==+1? strength-1 : strength;
+  strength = strengths.NZDCHF==+1? strength+1 : strength;
+  strength = strengths.NZDCHF==-1? strength-1 : strength;
+  strength = strengths.CHFJPY==+1? strength+1 : strength;
+  strength = strengths.CHFJPY==-1? strength-1 : strength;
+  strength = strengths.EURCHF==-1? strength+1 : strength;
+  strength = strengths.EURCHF==+1? strength-1 : strength;
+  currencyEls.chf.innerText = strength;
+}
+
+
+/* execute generateStrengthOutput when table is pressed */
+strengthTableEl = document.getElementById("strength-table");
+strengthTableEl.addEventListener("click", () => {
+  generateStrengthOutput();
+});
+
+/* Set strength value for each pair after clicking a cell in the table */
+cells = {
+  EURUSD: document.getElementById("EURUSD"),
+  GBPUSD: document.getElementById("GBPUSD"),
+  NZDUSD: document.getElementById("NZDUSD"),
+  AUDUSD: document.getElementById("AUDUSD"),
+  USDCAD: document.getElementById("USDCAD"),
+  USDCHF: document.getElementById("USDCHF"),
+  USDJPY: document.getElementById("USDJPY"),
+  EURGBP: document.getElementById("EURGBP"),
+  EURAUD: document.getElementById("EURAUD"),
+  EURCAD: document.getElementById("EURCAD"),
+  EURJPY: document.getElementById("EURJPY"),
+  EURNZD: document.getElementById("EURNZD"),
+  EURCHF: document.getElementById("EURCHF"),
+  GBPJPY: document.getElementById("GBPJPY"),
+  GBPNZD: document.getElementById("GBPNZD"),
+  GBPAUD: document.getElementById("GBPAUD"),
+  GBPCHF: document.getElementById("GBPCHF"),
+  GBPCAD: document.getElementById("GBPCAD"),
+  AUDNZD: document.getElementById("AUDNZD"),
+  AUDCAD: document.getElementById("AUDCAD"),
+  AUDCHF: document.getElementById("AUDCHF"),
+  AUDJPY: document.getElementById("AUDJPY"),
+  NZDCAD: document.getElementById("NZDCAD"),
+  NZDJPY: document.getElementById("NZDJPY"),
+  NZDCHF: document.getElementById("NZDCHF"),
+  CADJPY: document.getElementById("CADJPY"),
+  CADCHF: document.getElementById("CADCHF"),
+  CHFJPY: document.getElementById("CHFJPY")
+};
