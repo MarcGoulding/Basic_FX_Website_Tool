669a8c2511204804c22249fa6cf896770b856354
MarcGoulding <gouldingmarc@gmail.com>
2020-12-29 00:56:19 +0100

    buttons change tab that is visible

--- script.js
+++ script.js
@@ -35,13 +35,20 @@ b4.addEventListener("click", () => {
 
 /* ------------------------------------------------------------------------- */
 /* Create events to change tabs between FX and CRIPTO */
-let fx_tab = document.querySelector("FX-tab");
-let cripto_tab = document.querySelector("CRIPTO-tab");
-let fx_tab_button = document.querySelector("FX-tab-button");
-let cripto_tab_button = document.querySelector("CRIPTO-tab-button");
-// fx_tab_button.addEventListener("click", () => {
-//   console.log('clicked');
-// });
+let fx_tab_button = document.getElementById("FX-tab-button");
+let cripto_tab_button = document.getElementById("CRIPTO-tab-button");
+fx_tab_button.addEventListener("click", () => {
+  let fx_tab = document.getElementById("FX-tab");
+  let cripto_tab = document.getElementById("CRIPTO-tab");
+  fx_tab.style.display="none";
+  cripto_tab.style.display="block";
+});
+cripto_tab_button.addEventListener("click", () => {
+  let fx_tab = document.getElementById("FX-tab");
+  let cripto_tab = document.getElementById("CRIPTO-tab");
+  cripto_tab.style.display="none";
+  fx_tab.style.display="block";
+});
 
 /* ------------------------------------------------------------------------- */
 
